cd0a70f3aabad4d28c69026bece1fd120e0abc8b
Mathieu <mathieu.dumontier@oclock.io>
2022-03-13 01:55:15 +0100

    Try fix CORS

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ const dbAuthString = (DB_USERNAME && DB_PASSWORD) ? `${srvConfig.DB_USERNAME}:${
 
 let httpServer;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 /**
  * Configure middleware
  */
